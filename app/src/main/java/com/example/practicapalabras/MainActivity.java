0000d7751221acc4cba2f1cd6660363864dea0aa
joelbiznet <gerencia@idw.com.pe>
2019-08-26 18:18:12 -0500

    Idiomas

--- app/src/main/java/com/example/practicapalabras/MainActivity.java
+++ app/src/main/java/com/example/practicapalabras/MainActivity.java
@@ -6,10 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
