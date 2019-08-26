ec3356051ee2e763b3c87eaa47fce27c0ad1d617
joelbiznet <gerencia@idw.com.pe>
2019-08-25 23:40:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/practicapalabras/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.practicapalabras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText txt_uno;
+    public TextView contador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.txt_uno).requestFocus();
+        Button btn_contar = (Button)findViewById(R.id.btn_contar);
+        btn_contar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String cantidad = txt_uno.getText().toString();
+
+                String[] temp = cantidad.split(" ");
+                String uno = temp[0];
+                String dos = temp[1];
+
+                int cantidadNombres = temp.length;
+
+                if (temp[0].equals(temp[1])) { // strText is the string from the edit text, myString is the string
+                    Toast.makeText(this, "Las 2 palabras son iguales: ", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Las 2 palabras no son iguales: ", Toast.LENGTH_LONG).show();
+                }
+
+                Toast.makeText(this, "La cantidad de palabras es: " + cantidadNombres, Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+}
+
