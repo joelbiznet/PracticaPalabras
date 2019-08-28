f3941bef05dc70551a3e0fb1127de63a865d2124
joelbiznet <gerencia@idw.com.pe>
2019-08-27 21:22:35 -0500

    Mejorando

--- app/src/main/java/com/example/practicapalabras/MainActivity.java
+++ app/src/main/java/com/example/practicapalabras/MainActivity.java
@@ -3,18 +3,20 @@ package com.example.practicapalabras;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public EditText txt_uno;
-    private String[] vocales = {"U", "u", "i", "I"};
-
-    private int palabraEncontrada = 0;
-    private Button btn_contar;
+    EditText editText;
+    TextView textView;
+    int count;
+    int cantidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +24,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_contar.setOnClickListener(new View.OnClickListener() {
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
 
-            @Override
-            public void onClick(View v) {
+        editText.addTextChangedListener(new TextWatcher() {
 
-                for(String palabra: vocales){
-                    if(palabra.equals(txt_uno.getText().toString())){
-                        ++palabraEncontrada;
-                        break; //Si encuentra coincidencia detiene la ejecución del for.
-                    }
-                }
+        String vocal[] = {"U", "u", "i", "I"};
 
-                if(palabraEncontrada>1){ //se encontró una vocal.
-                    Toast.makeText(MainActivity.this, palabraEncontrada + " vocales cerradas encontradas",
-                            Toast.LENGTH_LONG).show();
-                }
-            }
+           @Override
+           public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+           }
+
+           @Override
+           public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+               String text = editText.getText().toString();
+               String[] textArray = text.split(" ");
+               cantidad = editText.getText().toString().length();
+
+               if (textArray.length==2) {
+                   for (i = 0; i<cantidad; i++) {
+
+                       if (text.equals(vocal[i])) {
+                        count++;
+                       }
+                   }
+                   textView.setText(count + " vocales cerradas");
+               }
+           }
+
+           @Override
+           public void afterTextChanged(Editable editable) {
+
+           }
         });
     }
 
